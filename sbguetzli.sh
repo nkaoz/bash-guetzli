d91d67a37e63c1032853c01032524ca1dd73c312
Neil Ruiz Dorador <nruizd.developer@gmail.com>
2017-04-12 18:10:04 -0500

    add title

--- sbguetzli.sh
+++ sbguetzli.sh
@@ -7,6 +7,11 @@ LIGHTGREEN="\033[1;32m"
 LIGHTBLUE="\033[1;34m"
 NC="\033[0m"
 
+printf "${LIGHTGREEN}******************************************\n"
+printf "sbGuetzli - bash compress jpg\n"
+printf "******************************************${NC}\n"
+
+
 if [ ! -d $FOLDER ]; then
   mkdir -p $FOLDER
 fi
@@ -14,7 +19,7 @@ fi
 for FILE in *.{jpg,jpeg,png} ; do
   if [ -f $FILE ]; then
     $GUETZLI --nomemlimit --verbose --quality 84 $FILE $FOLDER$FILE
-    printf "${LIGHTGREEN}Optimizado en ${NC} ${LIGHTBLUE}${FOLDER}${FILE}"
+    printf "${LIGHTGREEN}Optimizado en ${NC} ${LIGHTBLUE}${FOLDER}${FILE}${NC}"
   fi
 done
 
